6b3f2e8424410361d70d146b976821cdbb3e6d2d
Ramiro Aragon Paz <ramiroaragonpaz@gmail.com>
2022-03-28 11:19:04 -0300

    itemList added

--- src/components/ListProducts/Item.js
+++ src/components/ListProducts/Item.js
@@ -11,14 +11,14 @@ const Item = [
     {
         id: 2,
         title: "Plan 2", 
-        duration: "2 Mes",
+        duration: "2 Meses",
         description: "Plan Nutricional Completo. Incluye seguimiento perzonalizado y videollamadas de control ",
         price: 1000,
     },
     {
         id: 3,
         title: "Plan 3", 
-        duration: "3 Mes",
+        duration: "3 Meses",
         description: "Plan Nutricional Completo. Incluye seguimiento perzonalizado y videollamadas de control ",
         price: 1400,
     },
@@ -33,14 +33,14 @@ const Item = [
     {
         id: 5,
         title: "Recetario 2", 
-        duration: "2 Mes",
+        duration: "2 Meses",
         description: "Recetario Completo. Incluye 60 opciones de preparaciones con recetas y combinaciones posibles",
         price: 400,
     },
     {
         id: 6,
         title: "Plan Integral",
-        duration: "3 Mes",
+        duration: "3 Meses",
         description: "Plan Nutricional Completo. Incluye seguimiento perzonalizado, videollamadas de control y recetario completo el cual contiene mas de 60 opciones de preparaciones con recetas y combinaciones posibles", 
         price: 2000,
     }
